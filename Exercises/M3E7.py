d9efe5e8f4ea2b1e6d9df61fa5082e0d9fd06084
Heidi Pang <heidi.pang@oepfelbaum.com>
2024-04-13 14:18:56 +0200

    Integrated L-infinity convergence check

--- Exercises/M3E7.py
+++ Exercises/M3E7.py
@@ -7,28 +7,31 @@ b = np.array([24, 30, -24, 10, 20])
 w = 1.25
 
 
-def gauss_seidel(a, x, b, w, num_iterations, print_iterations=False):
-    # Finding length of a(n)
+def gauss_seidel(a, x, b, w, num_iterations, print_iterations=False, tol=1e-5):
     n = len(a)
 
     # Performing Gauss-Seidel iterations
     for k in range(num_iterations):
+        x_new = np.copy(x)  # Copy of x to check convergence by comparing x_new to the last iteration's x
         for j in range(n):
-            # Using temporary copy of x to prevent simultaneous update
-            temp_x = np.copy(x)
-
             # Calculate sum of a[j][i]*x[i] for all i != j
-            temp_sum = np.dot(a[j, :], temp_x) - a[j, j] * temp_x[j]
+            temp_sum = np.dot(a[j, :], x_new) - a[j, j] * x_new[j]
 
             # Update x[j] with relaxation
-            x[j] = (1 - w) * temp_x[j] + (w / a[j, j]) * (b[j] - temp_sum)
+            x_new[j] = (1 - w) * x_new[j] + (w / a[j, j]) * (b[j] - temp_sum)
 
         # Print x at each iteration if print_iterations is True
         if print_iterations:
-            print(f"After {k + 1} iterations, x = {x}")
+            print(f"After {k + 1} iterations, x = {x_new}")
+
+        # Check for convergence with L-infinity norm
+        if np.linalg.norm(x_new - x, np.inf) < tol:
+            print("Convergence achieved - no more iterations needed")
+            break
+        x = x_new
 
     return x
 
 
 initial_guess = np.array([1, 1, 1, 1, 1])
-x = gauss_seidel(A, initial_guess, b, w, 3, print_iterations=True)
+x = gauss_seidel(A, initial_guess, b, w, 18, print_iterations=True)
