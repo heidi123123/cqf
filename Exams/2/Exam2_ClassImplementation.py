17807ad5ebddc3be6acaa08f6360bc877a2dad14
Heidi Pang <heidi.pang@oepfelbaum.com>
2024-04-27 13:54:59 +0200

    Added variable plot_all_asian_options to make it customizable if ALL asian option prices are plotted

--- Exams/2/Exam2_ClassImplementation.py
+++ Exams/2/Exam2_ClassImplementation.py
@@ -64,13 +64,22 @@ class OptionPricer:
             lookback_fixed_call, lookback_fixed_put, lookback_float_call, lookback_float_put
 
 
-def plot_option_prices_vs_param(param_name, param_values, option_prices, annotation_precision=2, log_x_axis=False):
+def get_labels_for_plot(plot_all_asian_options):
+    arithmetic_labels = ['Asian Arithmetic Avg.\nFixed Call', 'Asian Arithmetic Avg.\n Fixed Put',
+                         'Asian Arithmetic Avg.\n Float Call', 'Asian Arithmetic Avg.\n Float Put']
+    geometric_labels = ['Asian Geometric Avg.\n Fixed Call', 'Asian Geometric Avg.\n Fixed Put',
+                        'Asian Geometric Avg.\n Float Call', 'Asian Geometric Avg.\n Float Put']
+    lookback_labels = ['Lookback Fixed Call', 'Lookback Fixed Put', 'Lookback Float Call', 'Lookback Float Put']
+    return arithmetic_labels + (geometric_labels if plot_all_asian_options else []) + lookback_labels
+
+
+def plot_option_prices_vs_param(param_name, param_values, option_prices, plot_all_asian_options,
+                                annotation_precision=2, log_x_axis=False):
     """Plot the option prices for different values of a parameter."""
     plt.figure(figsize=(12, 6))
 
     # define order of colors for the plots, so option price scatter dot has same color as the connecting line
     colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'rosybrown', 'darkorange', 'grey', 'lime', 'teal']
-
     for i, prices in enumerate(zip(*option_prices)):  # transpose option_prices list
         for value, price in zip(param_values, prices):
             plt.scatter(value, price, marker='o', color=colors[i])  # plot option price as scatter point
@@ -78,12 +87,7 @@ def plot_option_prices_vs_param(param_name, param_values, option_prices, annotat
             plt.text(value, price, f"{param_name}={str(round(value, annotation_precision))}", fontsize=8)
 
         # plot connecting lines for the scatter points of each option type, color=... ensures same color is used
-        label_list = ['Asian Arithmetic Avg.\nFixed Call', 'Asian Arithmetic Avg.\n Fixed Put',
-                      'Asian Arithmetic Avg.\n Float Call', 'Asian Arithmetic Avg.\n Float Put',
-                      'Asian Geometric Avg.\n Fixed Call', 'Asian Geometric Avg.\n Fixed Put',
-                      'Asian Geometric Avg.\n Float Call', 'Asian Geometric Avg.\n Float Put',
-                      'Lookback Fixed Call', 'Lookback Fixed Put',
-                      'Lookback Float Call', 'Lookback Float Put']
+        label_list = get_labels_for_plot(plot_all_asian_options)
         plt.plot(param_values, prices, label=label_list[i], color=colors[i])
 
     plt.xlabel(param_name)
@@ -96,68 +100,79 @@ def plot_option_prices_vs_param(param_name, param_values, option_prices, annotat
     plt.show()
 
 
-def analyze_number_of_mc_paths():
+def get_option_prices_to_plot(option_pricer_object, plot_all_asian_options):
+    all_option_prices = list(option_pricer_object.calculate_option_prices())
+    if not plot_all_asian_options:
+        option_prices = all_option_prices[:4] + all_option_prices[8:]
+    else:
+        option_prices = all_option_prices
+    return option_prices
+
+
+def analyze_number_of_mc_paths(plot_all_asian_options=0):
     param_name = "N"
     param_values = [10, 30, 60, 100, 300, 500, 1000, 5000, 10000, 20000, 50000, 100000, 1000000]
     option_prices = []
     for param_value in param_values:
         op = OptionPricer(S0=100, K=100, r=0.05, t=1, dt=0.01, sigma=0.2, number_of_mc_paths=param_value)
-        option_prices.append(op.calculate_option_prices())
-    plot_option_prices_vs_param(param_name, param_values, option_prices, log_x_axis=True)
+        option_prices.append(get_option_prices_to_plot(op, plot_all_asian_options))
+    plot_option_prices_vs_param(param_name, param_values, option_prices, plot_all_asian_options, log_x_axis=True)
 
 
-def analyze_initial_value():
+def analyze_initial_value(plot_all_asian_options=0):
     param_name = "S0"
     param_values = np.linspace(50, 150, 10 + 1)  # equidistant list of 10 values for S0 from 50 to 150
     option_prices = []
     for param_value in param_values:
         op = OptionPricer(S0=param_value, K=100, r=0.05, t=1, dt=0.01, sigma=0.2, number_of_mc_paths=20000)
-        option_prices.append(op.calculate_option_prices())
-    plot_option_prices_vs_param(param_name, param_values, option_prices, log_x_axis=False)
+        option_prices.append(get_option_prices_to_plot(op, plot_all_asian_options))
+    plot_option_prices_vs_param(param_name, param_values, option_prices, plot_all_asian_options, log_x_axis=False)
 
 
-def analyze_strike():
+def analyze_strike(plot_all_asian_options=0):
     param_name = "K"
     param_values = np.linspace(50, 150, 10 + 1)  # equidistant list of 10 values for K from 50 to 150
     option_prices = []
     for param_value in param_values:
         op = OptionPricer(S0=100, K=param_value, r=0.05, t=1, dt=0.01, sigma=0.2, number_of_mc_paths=20000)
-        option_prices.append(op.calculate_option_prices())
-    plot_option_prices_vs_param(param_name, param_values, option_prices)
+        option_prices = get_option_prices_to_plot(op, plot_all_asian_options)
+    plot_option_prices_vs_param(param_name, param_values, option_prices, plot_all_asian_options)
 
 
-def analyze_time_to_expiry():
+def analyze_time_to_expiry(plot_all_asian_options=0):
     param_name = "t"
     param_values = np.linspace(0.01, 5, 10 + 1)  # equidistant list of 10 values for t (time to expiry) from 0 to 1 year
     option_prices = []
     for param_value in param_values:
         op = OptionPricer(S0=100, K=100, r=0.05, t=param_value, dt=0.01, sigma=0.2, number_of_mc_paths=20000)
-        option_prices.append(op.calculate_option_prices())
-    plot_option_prices_vs_param(param_name, param_values, option_prices, annotation_precision=1)
+        option_prices = get_option_prices_to_plot(op, plot_all_asian_options)
+    plot_option_prices_vs_param(param_name, param_values, option_prices, plot_all_asian_options, annotation_precision=1)
 
 
-def analyze_time_step():
+def analyze_time_step(plot_all_asian_options=0):
     param_name = "dt"
     param_values = [0.001, 0.0025, 0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1]
     option_prices = []
     for param_value in param_values:
         op = OptionPricer(S0=100, K=100, r=0.05, t=1, dt=param_value, sigma=0.2, number_of_mc_paths=20000)
-        option_prices.append(op.calculate_option_prices())
-    plot_option_prices_vs_param(param_name, param_values, option_prices, annotation_precision=4, log_x_axis=True)
+        option_prices = get_option_prices_to_plot(op, plot_all_asian_options)
+    plot_option_prices_vs_param(param_name, param_values, option_prices, plot_all_asian_options, annotation_precision=4, log_x_axis=True)
 
 
-def analyze_volatility():
+def analyze_volatility(plot_all_asian_options=0):
     param_name = "sigma"
     param_values = np.linspace(0.01, 1, 10 + 1)  # an equidistant list of 10 values for sigma from 1% to 100%
     option_prices = []
     for param_value in param_values:
         op = OptionPricer(S0=100, K=100, r=0.05, t=1, dt=0.01, sigma=param_value, number_of_mc_paths=20000)
-        option_prices.append(op.calculate_option_prices())
-    plot_option_prices_vs_param(param_name, param_values, option_prices)
+        if not plot_all_asian_options:
+            all_option_prices = op.calculate_option_prices()
+            option_prices.append(all_option_prices[:4] + all_option_prices[8:])
+    plot_option_prices_vs_param(param_name, param_values, option_prices, plot_all_asian_options)
 
 
 def main():
-    analyze_initial_value()
+    analyze_initial_value(plot_all_asian_options=1)
 
 
 if __name__ == "__main__":
